284d8fd2efe09090d92c670be20eba85a9d79962
PerkinsZhu <perkinszhu@sinia.cn>
2019-06-02 18:50:35 +0800

    解决verticle 写大文件阻塞的问题

--- vertx-web-kotlin/src/main/kotlin/com/perkins/MainVerticle.kt
+++ vertx-web-kotlin/src/main/kotlin/com/perkins/MainVerticle.kt
@@ -18,7 +18,7 @@ class MainVerticle : AbstractVerticle() {
         val rxVerticle = RXVerticle()
         //TODO 当同时部署两个verticale的时候，第二个无法找到route，但是上传文件却可以成功的。
 //        vertx.deployVerticle(commonVertx) { completionHandler(it) }
-        vertx.deployVerticle(rxVerticle) { completionHandler(it) }
+        vertx.deployVerticle(routeVerticle) { completionHandler(it) }
 
     }
 
--- vertx-web-kotlin/src/main/kotlin/com/perkins/vertxs/RouteVerticle.kt
+++ vertx-web-kotlin/src/main/kotlin/com/perkins/vertxs/RouteVerticle.kt
@@ -2,9 +2,12 @@ package com.perkins.vertxs
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import io.vertx.core.Handler
+import io.vertx.core.WorkerExecutor
 import io.vertx.core.buffer.Buffer
 import io.vertx.core.logging.LoggerFactory
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.BodyHandler
 import java.io.File
 import java.io.FileInputStream
@@ -12,8 +15,11 @@ import java.nio.ByteBuffer
 
 
 class RouteVerticle : AbstractVerticle() {
+    lateinit var ioPool: WorkerExecutor // 先定义变量，延时初始化
     val logger = LoggerFactory.getLogger(this.javaClass)
     override fun start(startFuture: Future<Void>?) {
+
+        ioPool = vertx.createSharedWorkerExecutor("my-worker-pool", 10);
         val server = vertx.createHttpServer()
         val router = Router.router(vertx)
         router.route().handler(BodyHandler.create())
@@ -89,11 +95,30 @@ class RouteVerticle : AbstractVerticle() {
             }
             response.end()
         }
+        //使用work pool实现
+        router.get("/downloadFromBuffer2").handler(ioHandler)
 
-        //阻塞版下载
-        //TODO 无法处理并发任务
-        router.get("/downloadFromBuffer").blockingHandler { context ->
-            logger.info("接受到下载文件请求")
+
+        server.requestHandler(router::accept).listen(8080)
+    }
+
+
+    /**
+     *
+     * TODO 当并发数较多的时候会发生异常
+     *
+     * java.lang.IllegalStateException: Response is closed
+    at io.vertx.core.http.impl.HttpServerResponseImpl.checkValid(HttpServerResponseImpl.java:547)
+    at io.vertx.core.http.impl.HttpServerResponseImpl.write(HttpServerResponseImpl.java:581)
+    at io.vertx.core.http.impl.HttpServerResponseImpl.write(HttpServerResponseImpl.java:286)
+    at io.vertx.core.http.impl.HttpServerResponseImpl.write(HttpServerResponseImpl.java:53)
+     */
+    val ioHandler = Handler<RoutingContext> { context ->
+        logger.info("接受到下载文件请求")
+        // 开启线程池处理数据写操作
+        ioPool.executeBlocking<RoutingContext>({ future ->
+            logger.info("当前处理的请求-->" + context)
+            // 调用一些需要耗费显著执行时间返回结果的阻塞式API
             val id = context.queryParams().get("id")
             println("id=$id")
             val file = FileInputStream(File("file-uploads/${id}"))
@@ -105,15 +130,21 @@ class RouteVerticle : AbstractVerticle() {
             response.putHeader("Content-Disposition", "attachment;filename=data.mp4")
             response.setChunked(true)
             while (channel.read(byteBuffer) != -1) {
-                println("正在写数据.....")
                 byteBuffer.flip()
                 response.write(Buffer.buffer(byteBuffer.slice().array()))
                 byteBuffer.clear()
             }
-            response.end()
-        }
+            // 数据写入 response中之后结束future
+            future.complete(context)
+        }, { res ->
+            logger.info("数据读取结束")
+            //future结束之后，判断是否成功，如果执行成功，则关闭response
+            if (res.succeeded()) {
+                res.result().response().end()
+            } else {
+                logger.error("读取文件失败：", res.cause())
+            }
 
-
-        server.requestHandler(router::accept).listen(8080)
+        })
     }
 }
