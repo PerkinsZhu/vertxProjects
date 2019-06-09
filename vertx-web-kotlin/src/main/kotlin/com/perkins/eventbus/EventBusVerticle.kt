84b048426b820f52dedca280ba2ff6bc44743c5f
PerkinsZhu <perkinszhu@sinia.cn>
2019-06-09 18:39:22 +0800

    通过json格式传输二进制文件流

--- vertx-web-kotlin/src/main/kotlin/com/perkins/eventbus/EventBusVerticle.kt
+++ vertx-web-kotlin/src/main/kotlin/com/perkins/eventbus/EventBusVerticle.kt
@@ -1,15 +1,24 @@
 package com.perkins.eventbus
 
 import com.perkins.Runner
+import com.perkins.handlers.BaseHandle
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Handler
+import io.vertx.core.buffer.Buffer
+import io.vertx.core.eventbus.MessageConsumer
+import io.vertx.core.eventbus.impl.codecs.BufferMessageCodec
+import io.vertx.core.eventbus.impl.codecs.ByteArrayMessageCodec
 import io.vertx.core.http.HttpServerRequest
+import io.vertx.core.http.impl.WebSocketImplBase
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.handler.BodyHandler
 import io.vertx.ext.web.handler.StaticHandler
 import io.vertx.ext.web.handler.sockjs.BridgeOptions
 import io.vertx.ext.web.handler.sockjs.PermittedOptions
 import io.vertx.ext.web.handler.sockjs.SockJSHandler
+import java.sql.Blob
 import java.text.DateFormat
 import java.time.Instant
 import java.util.*
@@ -25,13 +34,29 @@ class EventBusVerticle : AbstractVerticle() {
     override fun start() {
         val router = Router.router(vertx)
         val opts = BridgeOptions()
-                .addOutboundPermitted(PermittedOptions().setAddress("feed"))
-                .addInboundPermitted(PermittedOptions().setAddress("feed"))
-                .addOutboundPermitted(PermittedOptions().setAddress("callback"))
-                .addInboundPermitted(PermittedOptions().setAddress("callback"))
+                .addOutboundPermitted(io.vertx.ext.bridge.PermittedOptions().setAddressRegex(".*"))
+                .addInboundPermitted(io.vertx.ext.bridge.PermittedOptions().setAddressRegex(".*"))
 
-        val ebHandler = SockJSHandler.create(vertx).bridge(opts)
+        val uploadFile = Handler<RoutingContext>{
+            val request = it.request()
+            request.params().map { BaseHandle.logger.info(it) }
+            request.headers().map { BaseHandle.logger.info(it) }
+            BaseHandle.logger.info(request.absoluteURI())
+
+            it.response().end("success")
+        }
+        uploadFile
+
+//        val da = WebSocketImplBase()
+        val ebHandler = SockJSHandler.create(vertx)
+                .bridge(opts)
+                .socketHandler{
+                    println(it.webSession().data())
+                    it.webUser()
+                }
+//                .handle()
         router.route("/eventbus/*").handler(ebHandler)
+//        router.route("/eventbus/*").handler(uploadFile)
 
         router.route().handler(StaticHandler.create())
 
@@ -40,15 +65,34 @@ class EventBusVerticle : AbstractVerticle() {
         val eb = vertx.eventBus()
 
 
+       val codec = ByteArrayMessageCodec()
+       println(codec.name())
+
         eb.consumer<String>("callback"){
             msg ->
-            println(msg.body())
+            println("body---"+msg.body())
+        }
+        val consumer = eb.consumer<JsonObject>("fileData"){
+            msg ->
+            println("fileData---"+msg.body())
         }
 
-        vertx.setPeriodic(1000L) { t ->
-            /*val timestamp = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM).format(Date.from(Instant.now()))
-            eb.send("feed", JsonObject().put("now", timestamp))*/
+        consumer.bodyStream().handler{
+            data ->
+            println(data)
+//            val contetn = data.getBinary("data")
+            val string = data.getString("data")
+//            println(contetn)
+            println(String(string.toByteArray(Charsets.UTF_16),Charsets.UTF_8))
+            println(String(string.toByteArray(Charsets.UTF_16BE),Charsets.UTF_8))
+            println(String(string.toByteArray(Charsets.UTF_16LE),Charsets.UTF_8))
+            print(data.javaClass)
         }
+
+        /*vertx.setPeriodic(1000L) { t ->
+            val timestamp = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM).format(Date.from(Instant.now()))
+            eb.send("feed", JsonObject().put("now", timestamp))
+        }*/
     }
 
 }
