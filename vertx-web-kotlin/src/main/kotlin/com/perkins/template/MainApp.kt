87fb7dbe68468c73e0b79e7ba777a8c62ad1c9a5
Perkins Zhu <perkinszhu@sina.cn>
2019-07-04 19:47:16 +0800

    some test

---
+++ vertx-web-kotlin/src/main/kotlin/com/perkins/template/MainApp.kt
@@ -0,0 +1,12 @@
+package com.perkins.template
+
+import io.vertx.ext.web.handler.TemplateHandler
+
+object MainApp {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+//        val handler = TemplateHandler.create(engine)
+
+    }
+}
