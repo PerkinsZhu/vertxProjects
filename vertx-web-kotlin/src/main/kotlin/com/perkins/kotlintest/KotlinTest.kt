643e0bb4b6a16c07962e4c4719808b39e1b63f5d
Perkins Zhu <perkinszhu@sina.cn>
2019-09-20 20:26:53 +0800

    some test

--- vertx-web-kotlin/src/main/kotlin/com/perkins/kotlintest/KotlinTest.kt
+++ vertx-web-kotlin/src/main/kotlin/com/perkins/kotlintest/KotlinTest.kt
@@ -1,15 +1,22 @@
 package com.perkins.kotlintest
 
+import com.hazelcast.client.impl.protocol.codec.AtomicReferenceIsNullCodec
 import com.perkins.bean.User
 import com.perkins.common.PropertiesUtil
 import com.perkins.util.DESUtils
 import io.vertx.core.json.JsonObject
 import org.apache.commons.codec.binary.Base64
 import org.junit.Test
+import rx.Single
+import rx.plugins.RxJavaCompletableExecutionHook
 import sun.misc.BASE64Encoder
 import java.security.SecureRandom
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.util.concurrent.atomic.AtomicInteger
 import java.util.regex.Matcher
 import java.util.regex.Pattern
+import kotlin.streams.toList
 
 
 class KotlinTest {
@@ -154,7 +161,7 @@ class KotlinTest {
     @Test
     fun getRegex() {
 //        val pattern = Pattern.compile("\\(*0\\d{2,3}\\)*-*\\d{7,8}")
-        val id_18= "([1-6][1-9]|50)\\d{4}(18|19|20)\\d{2}((0[1-9])|10|11|12)(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]"
+        val id_18 = "([1-6][1-9]|50)\\d{4}(18|19|20)\\d{2}((0[1-9])|10|11|12)(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]"
         val id_15 = "([1-6][1-9]|50)\\d{4}\\d{2}((0[1-9])|10|11|12)(([0-2][1-9])|10|20|30|31)\\d{3}"
         val phone = "(^\\d{15}\$)|(^\\d{18}\$)|(^\\d{17}(\\d|X|x)\$)"
 //        val mail = "^[A-Za-z0-9\\u4e00-\\u9fa5]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+\$"
@@ -166,13 +173,13 @@ class KotlinTest {
 //                "440204197911113613谁家" +
 //                "522528199008133616哈哈哈" +
                 "32031177070600" +
-                        "是3255133@qq.com"+
-                        "是skuher@qq.com.cn"+
-                        "是sku-her@qq.com.cn"+
-                        "是skuh.er@qq.com.cn"+
-                        "是12-we45564@qq.com"+
-                        "是kw.we-1245564@qq.com"+
-                        "是jd-l1245564@qq.com.cn"+
+                        "是3255133@qq.com" +
+                        "是skuher@qq.com.cn" +
+                        "是sku-her@qq.com.cn" +
+                        "是skuh.er@qq.com.cn" +
+                        "是12-we45564@qq.com" +
+                        "是kw.we-1245564@qq.com" +
+                        "是jd-l1245564@qq.com.cn" +
                         "是:3255133@qq.com"
         val str = "我的电话号码是：412325182303135417 sss 440204197911113613谁家 522528199008133616哈哈哈 32031177070600122撒的发生 320311770706002AAAA 320311770706002333 320311770706001BBBB 3203117707060012222 我的邮箱是：1245564@qq.com 我的邮箱是：12-we45564@qq.com 我的邮箱是：jd-l1245564@qq.com.cn 我的邮箱是：kw.we-1245564@qq.com"
         val pattern = Pattern.compile(mail)
@@ -186,12 +193,13 @@ class KotlinTest {
     }
 
     @Test
-    fun testReplace(){
+    fun testReplace() {
         val test = "kw.we-1245564@qq.com"
         val str = "我的邮箱是：1245564@qq.com 我的邮箱是：12-we45564@qq.com 我的邮箱是：jd-l1245564@qq.com.cn 我的邮箱是：kw.we-1245564@qq.com"
-        println(str.replace(test,""))
+        println(str.replace(test, ""))
 
     }
+
     @Test
     fun testDesUtil() {
         val password = PropertiesUtil.get("despassword")
@@ -210,17 +218,17 @@ class KotlinTest {
     }
 
     @Test
-    fun testJsonObject(){
-        val data = JsonObject().put("AA","11")
+    fun testJsonObject() {
+        val data = JsonObject().put("AA", "11")
         val str = data.getString("AA")
-        if(!str.isNullOrBlank()){
+        if (!str.isNullOrBlank()) {
             println(str)
         }
     }
 
 
     @Test
-    fun testParStream(){
+    fun testParStream() {
         val list = (1 until 10).toList()
         list.parallelStream().forEach {
             Thread.sleep(1000)
@@ -228,18 +236,81 @@ class KotlinTest {
         }
         println("==end")
 
-        val map = mutableMapOf<String,String>()
-        map.put("aa","ddd")
-        val a :String? =null
+        val map = mutableMapOf<String, String>()
+        map.put("aa", "ddd")
+        val a: String? = null
         println(map.get(a))
 
     }
 
 
     @Test
-    fun testCombin(){
-        val l1 = mutableListOf(1,2,3)
-        val l2 = mutableListOf(14,5,6)
+    fun testCombin() {
+        val l1 = mutableListOf(1, 2, 3)
+        val l2 = mutableListOf(14, 5, 6)
         println(l1 + l2)
+
+        val l3 = (0 until 103).toList()
+        val count = l3.size
+        for (i in 0 until count step 10) {
+            val end = if ((i + 10) < count) (i + 10) else {
+                count
+            }
+            val temp = l3.subList(i, end)
+            println(i)
+            println(temp)
+        }
+
     }
+
+
+    @Test
+    fun testPall() {
+        val list = (0 until 10).toList().parallelStream().map {
+            println(Thread.currentThread().name)
+            Thread.sleep((it % 3) * 1000L)
+            it * 10
+        }
+        println(list.toList())
+
+    }
+
+
+    @Test
+    fun testTime() {
+        val time = LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli()
+//        val time1= LocalDateTime.now().
+        println(time)
+        println(LocalDateTime.now().toEpochSecond(ZoneOffset.ofHours(8)))
+        println(LocalDateTime.now().toInstant(ZoneOffset.UTC).toString())
+        println(LocalDateTime.now().toInstant(ZoneOffset.ofHours(8)).toString())
+        println(LocalDateTime.now().toInstant(ZoneOffset.ofHours(8)).toEpochMilli())
+        println(LocalDateTime.now().toInstant(ZoneOffset.ofHours(0)).toEpochMilli())
+    }
+
+
+    @Test
+    fun testFuture() {
+        val count = AtomicInteger(0)
+        (0 until 100).toList().parallelStream().map {
+            val data = Single.just(it).map {
+                Thread.sleep(1000)
+                it
+            }.subscribe({
+                println("$it, end")
+                count.getAndIncrement()
+            }, {
+                count.getAndIncrement()
+                it.printStackTrace()
+            })
+            data
+        }.toList()
+
+        while (count.get() < 100) {
+            println("waiting……")
+            Thread.sleep(1000)
+        }
+        println("stop……")
+    }
+
 }
