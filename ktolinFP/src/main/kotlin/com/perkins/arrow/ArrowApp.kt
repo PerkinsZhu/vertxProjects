8d882a18152b4a2a570a97ed8d551a6ebfa9d4dd
Perkins Zhu <perkinszhu@sina.cn>
2019-11-07 18:39:49 +0800

    kotlin Arrow FP

--- ktolinFP/src/main/kotlin/com/perkins/arrow/ArrowApp.kt
+++ ktolinFP/src/main/kotlin/com/perkins/arrow/ArrowApp.kt
@@ -1,11 +1,16 @@
 package com.perkins.arrow
 
+import arrow.Kind
 import arrow.fx.IO
 import arrow.fx.extensions.fx
+import arrow.fx.extensions.io.concurrent.concurrent
 import arrow.fx.extensions.io.unsafeRun.runBlocking
+import arrow.fx.extensions.io.unsafeRun.unsafeRun
+import arrow.fx.typeclasses.Concurrent
+import arrow.fx.typeclasses.UnsafeRun
 import arrow.unsafe
 import com.perkins.AbstractApp
-import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.*
 import org.junit.Test
 
 class ArrowApp : AbstractApp() {
@@ -68,9 +73,119 @@ class ArrowApp : AbstractApp() {
         !effect { printThreadName() }
     }
 
+    @Test
     fun main2() { // The edge of our world
         unsafe { runBlocking { program } }
+        Thread.sleep(2000)
     }
 
+    suspend fun threadName(): String = Thread.currentThread().name
+    val program3 = IO.fx {
+        val fiberA = !effect {
+            logger.info("start fiber A")
+            Thread.sleep(5000)
+            threadName()
+        }.map {
+            23
+        }.fork(dispatchers().default())
+        val fiberB = !effect { threadName() }.fork(dispatchers().default())
+        val threadA = !fiberA.join()
+        val threadB = !fiberB.join()
+        !effect { logger.info(threadA.toString()) }
+        !effect { logger.info(threadB) }
+    }
+
+    @Test
+    fun main3() { // The edge of our world
+        unsafe { runBlocking { program3 } }
+    }
+
+
+    data class ThreadInfo(
+            val threadA: String,
+            val threadB: String
+    )
+
+    val program4 = IO.fx {
+        val (threadA: String, threadB: String) =
+                !dispatchers().default().parMapN(
+                        effect { threadName() },
+                        effect { threadName() },
+                        ::ThreadInfo
+                )
+        !effect { println(threadA) }
+        !effect { println(threadB) }
+    }
+
+    @Test
+    fun main4() { // The edge of our world
+        unsafe { runBlocking { program4 } }
+    }
+
+
+    suspend fun threadName(i: Int): String =
+            "$i on ${Thread.currentThread().name}"
+
+    val program5 = IO.fx {
+        val result: List<String> = !listOf(1, 2, 3).parTraverse { i -> effect { threadName(i) } }
+        val result2: List<String> = listOf(1, 2, 3).parTraverse { i -> effect { threadName(i) } }.bind()
+
+        !effect { result.forEach { logger.info(it) } }
+
+    }
+
+    @Test
+    fun main5() { // The edge of our world
+        unsafe { runBlocking { program5 } }
+    }
+
+
+    val program6 = IO.fx {
+        val result: List<String> = !listOf(
+                effect { threadName() },
+                effect { threadName() },
+                effect { threadName() }
+        ).parSequence()
+        !effect { println(result) }
+    }
+
+    @Test
+    fun main6() { // The edge of our world
+        unsafe { runBlocking { program6 } }
+    }
+
+    suspend fun program() = GlobalScope.async { printThreadName() }
+
+    @Test
+    fun main7() {
+        runBlocking { program().await() }
+    }
+
+    suspend fun program7() = GlobalScope.async(start = CoroutineStart.LAZY) { printThreadName() }
+
+    @Test
+    fun main8() {
+        runBlocking { program7().await() }
+    }
+
+    /* a side effect */
+    val const = 1
+
+    suspend fun sideEffect(): Int {
+        println(Thread.currentThread().name)
+        return const
+    }
+
+    /* for all `F` that provide an `Fx` extension define a program function */
+    fun <F> Concurrent<F>.program(): Kind<F, Int> = fx.concurrent { !effect { sideEffect() } }
+
+    /* for all `F` that provide an `UnsafeRun` extension define a main function */
+    fun <F> UnsafeRun<F>.main(fx: Concurrent<F>): Int = unsafe { runBlocking { fx.program() } }
+
+    /* Run program in the IO monad */
+    @Test
+    fun main9() {
+        IO.unsafeRun().main(IO.concurrent())
+    }
 
 }
--- ktolinFP/src/main/kotlin/com/perkins/coroutines/CoroutinesApp.kt
+++ ktolinFP/src/main/kotlin/com/perkins/coroutines/CoroutinesApp.kt
@@ -1,11 +1,16 @@
 package com.perkins.coroutines
 
+import arrow.fx.extensions.io.concurrent.sleep
 import com.perkins.AbstractApp
 import kotlinx.coroutines.*
 import org.junit.Test
+import kotlin.concurrent.thread
+import kotlin.coroutines.CoroutineContext
+import kotlin.math.log
+import kotlin.time.Duration
 
 class CoroutinesApp : AbstractApp() {
-
+    private fun log(data: String) = logger.info("-------$data")
     //协程测试
     @Test
     fun coroutineDemo1() {
@@ -132,6 +137,193 @@ class CoroutinesApp : AbstractApp() {
             val response = getResponse(token)
             setText(response)
         }
+
+        thread {
+            logger.info("-----i am thread")
+        }.start()
+
+        Thread.sleep(2000)
+
+    }
+
+
+    @Test
+    fun main() {
+        /*(0 until 10).toList().parallelStream().forEach {
+            GlobalScope.launch {
+                // 在后台启动一个新的协程并继续
+                delay(1000L)
+                logger.info("---$it")
+            }
+        }*/
+        (0 until 10).toList().forEach {
+            log("---$it")
+//            GlobalScope.launch(Dispatchers.Unconfined) {
+            GlobalScope.launch {
+                // 协程内部是顺序串行的，协程之间如果不是在同一个线程中，则回事异步的，在同一个线程中则是同步的
+                // 在后台启动一个新的协程并继续
+//                delay(1000L)
+                (0 until 10).forEach { item ->
+                    log("$it---$item")
+                }
+                log("=======$it")
+            }
+        }
+        println("Hello,") // 主线程中的代码会立即执行
+        runBlocking {
+            // 但是这个表达式阻塞了主线程
+            delay(2000L)  // ……我们延迟 2 秒来保证 JVM 的存活
+        }
+    }
+
+    @Test
+    fun testAppScop() {
+        runBlocking {
+            GlobalScope.launch {
+                log("main launch")
+                repeat(1000) { i ->
+                    log("I'm sleeping $i ...")
+                    delay(500L) //这里的delay会导致线程切换
+                }
+            }
+            delay(5300L) // 在延迟后退出
+        }
+    }
+
+    @Test
+    fun testManyCoroutines() = runBlocking {
+        repeat(100_000) {
+            launch {
+                delay(1000L)
+                log(it.toString())
+            }
+        }
+    }
+
+    @Test
+    fun testCancel() {
+        runBlocking {
+            val job = launch {
+                repeat(1000) { i ->
+                    println("job: I'm sleeping $i ...")
+                    delay(500L)
+                }
+            }
+            delay(1300L) // 延迟一段时间
+            log("main: I'm tired of waiting!")
+            job.cancel() // 取消该作业
+            log("im cancel")
+            delay(1000)
+            log("im join")
+            job.join() // 等待作业执行结束
+            println("main: Now I can quit.")
+        }
+    }
+
+    @Test
+    fun coroutineContextTest() = runBlocking<Unit> {
+        launch {
+            // 运行在父协程的上下文中，即 runBlocking 主协程
+            println("main runBlocking      : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(Dispatchers.Unconfined) {
+            // 不受限的——将工作在主线程中
+            println("Unconfined            : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(Dispatchers.Default) {
+            // 将会获取默认调度器
+            println("Default               : I'm working in thread ${Thread.currentThread().name}")
+        }
+        launch(newSingleThreadContext("MyOwnThread")) {
+            // 将使它获得一个新的线程
+            println("newSingleThreadContext: I'm working in thread ${Thread.currentThread().name}")
+        }
+    }
+
+    @Test
+    fun testDebug() = runBlocking<Unit> {
+        val a = async {
+            log("I'm computing a piece of the answer")
+            6
+        }
+        val b = async {
+            log("I'm computing another piece of the answer")
+            7
+        }
+        log("The answer is ${a.await() * b.await()}")
+    }
+
+
+    @Test
+    fun switchContext() {
+        newSingleThreadContext("Ctx1").use { ctx1 ->
+            newSingleThreadContext("Ctx2").use { ctx2 ->
+                runBlocking(ctx1) {
+                    log("Started in ctx1")
+                    withContext(ctx2) {
+                        log("Working in ctx2")
+                    }
+                    log("Back to ctx1")
+                }
+            }
+        }
+    }
+
+    @Test
+    fun testThread() {
+        runBlocking {
+            log("i am run blocking ")
+//            GlobalScope.launch { // 会在新的线程中执行
+//            launch { //和blocking线程一致，代码会阻塞串行执行
+//            this.launch {//和blocking线程一致，代码会阻塞串行执行
+//            val  a = this.async<Int> {//和blocking线程一致，代码会阻塞串行执行 ,该方法会有返回值
+            async(Dispatchers.Default) {
+                //和blocking线程一致，代码会阻塞串行执行
+                repeat(10) { i ->
+                    log("---$i")
+                    delay(1000) // delay会导致线程切换
+                    log("---$i--END")
+                }
+            }
+            log("end blocking ")
+            delay(10000)
+        }
+    }
+
+
+    @Test
+    fun testAsync2() {
+        GlobalScope.launch {
+            val data = this.async {
+                (0 until 100).fold(10) { a, b -> a + b }
+            }
+            val result = data.await()
+            log("result :$result")
+        }
+        Thread.sleep(2000)
+    }
+
+    @Test
+    fun testCoroutineThread() {
+        runBlocking {
+            repeat(100) {
+                Thread {
+                    log("---$it")
+                }.start()
+            }
+        }
+    }
+}
+
+
+object AAA : AbstractApp() {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Thread {
+            logger.info("---")
+        }.start()
+
         Thread.sleep(2000)
     }
 
