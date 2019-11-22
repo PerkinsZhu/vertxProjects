b318c65577e25d38e21523b8cf7cc5e84e2cd1f3
Perkins Zhu <perkinszhu@sina.cn>
2019-11-22 20:35:08 +0800

    add groovy module

---
+++ GroovyStudy/src/HelloWorld.groovy
@@ -0,0 +1,11 @@
+import groovy.xml.MarkupBuilder
+
+class HelloWorld {
+    static void main(args) {
+        println("-------------")
+    }
+    def employeename
+    def student1
+    def student_name
+    def xml = new MarkupBuilder()
+}
