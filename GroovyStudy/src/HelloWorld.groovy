7d223a118254abf9dff5d1fd59e4f740bdcb2b3a
Perkins <perkinszhu@sina.cn>
2019-11-24 16:11:53 +0800

    groovy study

--- GroovyStudy/src/HelloWorld.groovy
+++ GroovyStudy/src/HelloWorld.groovy
@@ -3,9 +3,20 @@ import groovy.xml.MarkupBuilder
 class HelloWorld {
     static void main(args) {
         println("-------------")
+
+        def a = 1..10
+        a.each { i ->
+            println("${i}")
+        }
+        (1..10).each { i ->
+            println("${i}")
+        }
+
     }
     def employeename
     def student1
     def student_name
     def xml = new MarkupBuilder()
+
+
 }
