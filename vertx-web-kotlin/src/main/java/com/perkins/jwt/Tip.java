35adaf907c9c6eb64825bdf7be7562e437b14a6f
Perkins Zhu <perkinszhu@sina.cn>
2019-11-28 18:00:00 +0800

    添加 可以生成 加密的 jwt工具类

---
+++ vertx-web-kotlin/src/main/java/com/perkins/jwt/Tip.java
@@ -0,0 +1,10 @@
+package com.perkins.jwt;
+
+public class Tip {
+    //https://github.com/sililau/jwt-rsa
+    //RSA加密示例    https://github.com/sililau/jwt-rsa/blob/master/src/test/java/AesToken.java
+    //先生成公钥和私钥
+    //https://github.com/sililau/jwt-rsa/blob/master/src/test/java/BuildRsa.java
+    //然后想办法生成keystore.jceks
+    //之后执行RSA加密demo
+}
